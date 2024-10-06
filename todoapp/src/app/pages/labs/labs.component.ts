19b1cda62d3c43ac943efe46cea8b94de1f6bd60
Geffry Alejandro Ospina Atehortúa <geffry.ospina@gmail.com>
2024-10-06 10:10:19 -0500

    clase: event bindings

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -13,5 +13,31 @@ export class LabsComponent {
 
   onButtonClick() {
     this.buttonValue = !this.buttonValue;
+    alert('Button clicked');
   }
+  onButtonDoubleClick(){
+    alert('Button double clicked');
+  }
+
+  person = {
+    name: 'John Doe',
+    age: 30,
+    address: {
+      street: '123 Main St',
+      city: 'Boston',
+      state: 'MA'
+    }
+  };
+  // algunos pueden recibir eventos
+  onAgeChange(event: Event) {
+   console.log(event); 
+  }
+  // para controlar los eventos de teclado se maneja con este tipo de eventos
+  onKeyDown(event: KeyboardEvent) {
+    alert(`onkeydown event ${event.key}`);
+  }
+  onKeyUp(event: KeyboardEvent) {
+    alert(`onkeyup event ${event.key}`);
+  }
+
 }
